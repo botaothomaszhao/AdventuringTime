f8696b68d8c7acd6522415d05fe7b1e8fb9a36e5
dev <dev@local>
2026-08-05 22:08:55 +0800

    应用标题改为中文'探索的时光'（Windows 窗口/资源/Android label）

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -27,7 +27,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.Create(L"adventuring_time", origin, size)) {
+  if (!window.Create(L"\u63a2\u7d22\u7684\u65f6\u5149", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
